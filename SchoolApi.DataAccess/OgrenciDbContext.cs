7272c2aaa4371c93c2aca94018311511d91879f8
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2024-11-01 11:55:37 +0300

    Proje dosyası ekle.

---
+++ SchoolApi.DataAccess/OgrenciDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApi.DataAccess
+{
+    public class OgrenciDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Database=Ogrenci; integrated security=true;");
+        }
+        public DbSet<Ogrenci> Ogrencis { get; set; }
+    }
+}
